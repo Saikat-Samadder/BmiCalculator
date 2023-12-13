bb85a01eaa5bc48ed9f3a3c4503dbf461fa5d1f5
isaikat <www.isaikat98@gmail.com>
2023-12-13 22:03:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[weight,setweight] = useState(0)
+  const[height,setheight] = useState(0)
+  const[bmi,setbmi] = useState('')
+  const[massage,setMassage] = useState('') 
+
+  function calcbmi(e){
+   e.preventDefault()
+   if(height==0 || weight==0){
+    alert("Please enter a valid weight and height")
+   }else{
+     let bmi = (weight/(height*height)*703)
+     setbmi(bmi.toFixed(1))
+
+     if(bmi<25){
+      setMassage("you are under weight")
+     }else if(bmi>=25 && bmi<30){
+        setMassage("You are a healthy weight")
+     }else{
+      setMassage("You are a overweight")
+     }
+   }
+  }
+ 
+let Reload =()=>{
+  window.location.reload()
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div className="app">
+    <div className="container">
+    <h2>BMI Calculator</h2>
+    <form onSubmit={calcbmi}>
+      <div>
+        <label>Weight (ibs)</label>
+        <input type="text" placeholder='Enter Weight Value' value={weight} onChange={(e)=>setweight(e.target.value)}/>
+      </div>
+      <div>
+        <label>Height (in)</label>
+        <input type="text" placeholder='Enter Weight Value' value={height} onChange={(e)=>setheight(e.target.value)}/>
+      </div>
+       <div>
+       <button className='btn'type='submit'>Submit</button>
+       <button className='btn btn-outline' type='submit'onClick={Reload}>Reload</button>
+       </div>
+       <div className="center">
+        <h3>Your BMI id {bmi}</h3>
+        <p>{massage}</p>
+       </div>
+    </form>
     </div>
+    </div>
+   </>
   );
 }
 
